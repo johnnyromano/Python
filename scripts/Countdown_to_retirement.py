57e740d8cd67f40d6a6ed8aee9584a5f5614267d
Johnny Romano <john.p.romano@gmail.com>
2017-06-28 23:01:10 -0700

    added countdown to retirement script

---
+++ scripts/Countdown_to_retirement.py
@@ -0,0 +1,11 @@
+print("When do you retire...")
+year = int(input("What year where you born? "))
+years = 2017 - year
+days = years * 365
+
+if days > 20000:
+    print("Wow, you're old it's been {} days, that's {} years".format(days,years))
+elif days > 10000:
+    print("Lots of time left it's been {} days, that's {} years".format(days,years))
+else:
+    print("Time to get started it's only been {} days, that's {} years".format(days,years))
