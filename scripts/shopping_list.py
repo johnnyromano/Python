8642e5d33754a446c31432b0b78a537beaaea920
Johnny Romano <john.p.romano@gmail.com>
2017-07-11 11:41:04 -0700

    added simple CL list functions

--- scripts/shopping_list.py
+++ scripts/shopping_list.py
@@ -1,6 +1,22 @@
 # make a list to hold onto items
-# inform user on how to use the app
-# ask for new items
-# add new items to the list
-# allow for quiting app
+shopping_list = []
+
+# print out instructions on how to use the app
+print("What do we need to get while we're out?")
+print("Enter 'DONE' to stop adding items.")
+
+while True:
+    # ask for new items
+    new_item = input("> ")
+
+    # allow for quiting app
+    if new_item == 'DONE':
+         break
+
+    # add new items to the list
+    shopping_list.append(new_item)
+
 # print out the list
+print("Here's your list:")
+for item in shopping_list:
+    print(item)
