9c4e215cb42aa86dfa252234f9667038c5d0e745
Johnny Romano <john.p.romano@gmail.com>
2017-07-11 12:59:41 -0700

    Added basic hints

---
+++ scripts/number_game.py
@@ -0,0 +1,19 @@
+#Author: Johnny Romano
+
+import random
+
+# generate a number between 1 and 10
+secret_num = random.randint(1, 10)
+
+while True:
+# get a number guess from the player
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    # compare guess to secret number
+    if guess == secret_num:
+        print("You got it! My number was {}".format(secret_num))
+        break
+    elif guess > secret_num:
+        print("Sorry, try lower!")
+    elif guess < secret_num:
+        print("Sorry, try higher!")
