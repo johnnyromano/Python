31b1df170ae55f556905c08b6d4697545945cb72
Johnny Romano <john.p.romano@gmail.com>
2017-07-12 12:49:19 -0700

    Added prompt to start game again

--- scripts/number_game.py
+++ scripts/number_game.py
@@ -1,25 +1,35 @@
 #Author: Johnny Romano
 import random
 
-# generate a number between 1 and 10
-secret_num = random.randint(1, 10)
-guesses = []
+def game():
+    # generate a number between 1 and 10
+    secret_num = random.randint(1, 10)
+    guesses = []
 
-def main():
     while len(guesses) < 5:
         try:
         # get a number guess from the player
-            guess = int(input("Guess a number between 1 and 10: "))
+            guess = int(input("\nGuess a number between 1 and 10: "))
+        # catch if not number
         except ValueError:
             print("{} isn't a number!".format(guess))
         else:
+            # add guess to guesses
+            guesses.append(guess)
             # compare guess to secret number
             if guess == secret_num:
-                print("You got it! My number was {}".format(secret_num))
+                print("You got it! {} was my number.\nIt took you {} attempts".format(secret_num, len(guesses)))
                 break
             elif guess > secret_num:
-                print("Sorry, try lower!")
+                print("Try lower than {}!".format(guess))
             elif guess < secret_num:
-                print("Sorry, try higher!")
-            guesses.append(guess)
-main()
+                print("Try higher than {}!".format(guess))
+    else:
+        print("\nYou didn't get it! My number was {}.".format(secret_num))
+
+    play_again = input("\nDo you want to play again? Y/n ")
+    if play_again.lower() != 'n':
+        game()
+    else:
+        print("See ya later!")
+game()
