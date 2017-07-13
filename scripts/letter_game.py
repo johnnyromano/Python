8a5cd9eec286b1bbb9bf0653a1da751b14c1346e
Johnny Romano <john.p.romano@gmail.com>
2017-07-13 14:25:04 -0700

    added header block

---
+++ scripts/letter_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+    File name: letter_game.py
+    Description: Hangman based letter guessing game.
+    Author: Johnny Romano
+    Email: John.p.romano@gmail.com
+    Date created: 13-July-2017
+    Date last modified: 13-July-2017
+    Python Version: 3.5
+"""
