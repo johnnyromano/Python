3faada11c126d03679cfe6edbdf4910994865043
Johnny Romano <john.p.romano@gmail.com>
2017-07-26 00:23:51 -0700

    initial commit catacombs_game.py

---
+++ scripts/catacombs_game.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+    File name: castle_game.py
+    Description: Navigate the treacherous hidden catacombs.
+    Author: Johnny Romano
+    Email: John.p.romano@gmail.com
+    Date created: 26-July-2017
+    Date last modified: 26-July-2017
+    Python Version: 3.5
+"""
